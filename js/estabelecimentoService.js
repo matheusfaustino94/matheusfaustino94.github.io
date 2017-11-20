ce9bb3ee50d3bfa29ccac6e44457a8d0010e74e4
Matheus Faustino <matheusfaustino94@live.com>
2017-11-20 19:48:51 -0300

    corrigindo bug dos vários estabelecimentos

--- js/estabelecimentoService.js
+++ js/estabelecimentoService.js
@@ -2,11 +2,13 @@
 $(document).ready(function(){
     $(".novoProduto").click(function(){
         $.get("https://projeto-aeso.herokuapp.com/api/estabelecimentos", function(result){           
-            var options = $('#estabelecimento').html();
-            $.each(result, function(i, field){
-                options = options + '<option value='+field._id+'>'+field.nomeEstabelecimento+'</option>';             
-            });
-            $('#estabelecimento').html(options);
+            if($('#estabelecimento option').length < 2){
+                var options = $('#estabelecimento').html();
+                $.each(result, function(i, field){
+                    options = options + '<option value='+field._id+'>'+field.nomeEstabelecimento+'</option>';             
+                });
+                $('#estabelecimento').html(options);
+            }
         });
     });
 });
